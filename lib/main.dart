070963e652ce4cb8702e6e432b4236c932a0d961
eyakub <eyakubsorkar@gmail.com>
2019-03-30 00:47:20 +0600

    #LOGIN & SIgnUP page added

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:scoped_model/scoped_model.dart';
-import 'pages/login_page.dart';
+import './pages/login_page.dart';
+import './pages/signup.dart';
 import './pages/home_page.dart';
-import './pages/splash_page.dart';
+//import './pages/splash_page.dart';
 import './pages/contest_list.dart';
 import './pages/user_profile_page.dart';
 import './pages/contest_wishlist.dart';
 import './pages/contest_submissions.dart';
 import './pages/contest_details.dart';
 import './pages/test.dart';
-
 import './pages/create_contest.dart';
+
 //import 'package:flutter/rendering.dart';
 
 import './scoped_models/main_scoped_model.dart';
 
-
 void main() {
   //debugPaintSizeEnabled = true;
-   //debugPaintBaselinesEnabled = true;
+  //debugPaintBaselinesEnabled = true;
   // debugPaintPointersEnabled = true;
   runApp(MyApp());
 }
@@ -45,15 +45,17 @@ class _MyAppState extends State<MyApp> {
           fontFamily: 'Nunito',
         ),
         routes: {
-          '/': (BuildContext context) => SplashPage(_model),
-          '/login': (BuildContext context) => LoginPage(_model),
+          // '/': (BuildContext context) => SplashPage(_model),
+          '/': (BuildContext context) => LoginPage(_model),
+          '/signup': (BuildContext context) => SignupPage(),
           '/home': (BuildContext context) => HomePage(_model),
           '/contests': (BuildContext context) => ContestListPage(),
-          '/userProfile':(BuildContext context) => UserProfilePage(),
+          '/userProfile': (BuildContext context) => UserProfilePage(),
           '/createContest': (BuildContext context) => CreateContest(),
           '/contestDetails': (BuildContext context) => ContestDetailsPage(),
           '/contestWishlist': (BuildContext context) => ContestWishlistPage(),
-          '/contestSubmissions': (BuildContext context) => ContestSubmissionPage(),
+          '/contestSubmissions': (BuildContext context) =>
+              ContestSubmissionPage(),
           '/test': (BuildContext context) => Test(),
         },
         onGenerateRoute: (RouteSettings settings) {
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -49,7 +49,7 @@ class _LoginPageState extends State<LoginPage> {
           borderRadius: BorderRadius.circular(24),
         ),
         onPressed: () {
-          //Navigator.of(context).pushNamed(HomePage.tag);
+          Navigator.pushReplacementNamed(context, '/home');
         },
         padding: EdgeInsets.all(12),
         color: Colors.lightBlueAccent,
@@ -65,24 +65,149 @@ class _LoginPageState extends State<LoginPage> {
       onPressed: () {},
     );
 
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Center(
-        child: ListView(
-          shrinkWrap: true,
-          padding: EdgeInsets.only(left: 24.0, right: 24.0),
+    // return Scaffold(
+    //   backgroundColor: Colors.white,
+    //   body: Center(
+    //     child: ListView(
+    //       shrinkWrap: true,
+    //       padding: EdgeInsets.only(left: 24.0, right: 24.0),
+    //       children: <Widget>[
+    //         logo,
+    //         SizedBox(height: 48.0),
+    //         email,
+    //         SizedBox(height: 8.0),
+    //         password,
+    //         SizedBox(height: 24.0),
+    //         loginButton,
+    //         forgotLabel
+    //       ],
+    //     ),
+    //   ),
+    // );
+    return new Scaffold(
+        resizeToAvoidBottomPadding: false,
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            logo,
-            SizedBox(height: 48.0),
-            email,
-            SizedBox(height: 8.0),
-            password,
-            SizedBox(height: 24.0),
-            loginButton,
-            forgotLabel
+            Container(
+              child: Stack(
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.fromLTRB(15.0, 110.0, 0.0, 0.0),
+                    child: Text('Hello',
+                        style: TextStyle(
+                            fontSize: 80.0, fontWeight: FontWeight.bold)),
+                  ),
+                  Container(
+                    padding: EdgeInsets.fromLTRB(16.0, 175.0, 0.0, 0.0),
+                    child: Text('There',
+                        style: TextStyle(
+                            fontSize: 80.0, fontWeight: FontWeight.bold)),
+                  ),
+                  Container(
+                    padding: EdgeInsets.fromLTRB(220.0, 175.0, 0.0, 0.0),
+                    child: Text('.',
+                        style: TextStyle(
+                            fontSize: 80.0,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.green)),
+                  )
+                ],
+              ),
+            ),
+            Container(
+                padding: EdgeInsets.only(top: 35.0, left: 20.0, right: 20.0),
+                child: Column(
+                  children: <Widget>[
+                    TextField(
+                      decoration: InputDecoration(
+                          labelText: 'EMAIL',
+                          labelStyle: TextStyle(
+                              fontFamily: 'Montserrat',
+                              fontWeight: FontWeight.bold,
+                              color: Colors.grey),
+                          focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.green))),
+                    ),
+                    SizedBox(height: 20.0),
+                    TextField(
+                      decoration: InputDecoration(
+                          labelText: 'PASSWORD',
+                          labelStyle: TextStyle(
+                              fontFamily: 'Montserrat',
+                              fontWeight: FontWeight.bold,
+                              color: Colors.grey),
+                          focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.green))),
+                      obscureText: true,
+                    ),
+                    SizedBox(height: 5.0),
+                    Container(
+                      alignment: Alignment(1.0, 0.0),
+                      padding: EdgeInsets.only(top: 15.0, left: 20.0),
+                      child: InkWell(
+                        child: Text(
+                          'Forgot Password',
+                          style: TextStyle(
+                              color: Colors.green,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'Montserrat',
+                              decoration: TextDecoration.underline),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 40.0),
+                    Container(
+                      height: 40.0,
+                      child: Material(
+                        borderRadius: BorderRadius.circular(20.0),
+                        shadowColor: Colors.greenAccent,
+                        color: Colors.green,
+                        elevation: 7.0,
+                        child: GestureDetector(
+                          onTap: () {
+                            Navigator.pushReplacementNamed(context, '/home');
+                          },
+                          child: Center(
+                            child: Text(
+                              'LOGIN',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontFamily: 'Montserrat'),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20.0),
+                  ],
+                )),
+            SizedBox(height: 15.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  "Don't have any account ?",
+                  style: TextStyle(fontFamily: 'Montserrat'),
+                ),
+                SizedBox(width: 5.0),
+                InkWell(
+                  onTap: () {
+                    Navigator.pushReplacementNamed(context, '/signup');
+                  },
+                  child: Text(
+                    'Register',
+                    style: TextStyle(
+                        color: Colors.green,
+                        fontFamily: 'Montserrat',
+                        fontWeight: FontWeight.bold,
+                        decoration: TextDecoration.underline),
+                  ),
+                )
+              ],
+            )
           ],
-        ),
-      ),
-    );
+        ));
   }
 }
